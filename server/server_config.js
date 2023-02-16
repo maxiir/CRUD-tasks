682157d84d557540b9b1c395e2e9f696390169e5
Maxi <maxiirucci@gmail.com>
2023-02-16 00:59:15 -0300

    new commit

--- server/server_config.js
+++ server/server_config.js
@@ -11,16 +11,16 @@ loadDotenv()
 const app = express()
 const __dirname= dirname(fileURLToPath(import.meta.url))
 const stateProyect = process.env.STATE === 'production'
-export const port = process.env.PORT || 5000
+export const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors())
 app.use(router)
 
 if(stateProyect){
-    app.use(express.static(join(__dirname,'./src/dist')))
+    app.use(express.static(join(__dirname,'../server/src/dist')))
     app.get('*',(req,res) =>{
-        const index = join(__dirname,'./src/dist/index.html')
+        const index = join(__dirname,'./index.html')
         res.sendFile(index,(err)=>{
             if(err){
                 res.status(500).json({err:'error con el envio de archivos'})
